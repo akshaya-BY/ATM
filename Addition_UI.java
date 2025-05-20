3a8fe2fb6000525ae889f8e648a3494b032219f5
Akshaya P (CW) <Akshaya.P@jda.com>
2025-05-20 15:18:36 +0530

    Addition_UI

---
+++ Addition_UI.java
@@ -0,0 +1,22 @@
+
+
+import java.util.Scanner;
+
+public class Addition_UI {
+    public static void main(String[] args) {
+        Scanner addition = new Scanner(System.in);
+
+        // Getting user input for 1st number
+        System.out.println("Enter 1st number: ");
+        int a = addition.nextInt();
+
+        // Getting user input for 2nd number
+        System.out.println("Enter 2nd number: ");
+        int b = addition.nextInt();
+        
+        System.out.println( a + b);
+        
+        addition.close();
+        // Closing the scanner object to prevent resource leaks
+}
+}
